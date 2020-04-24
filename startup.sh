64923f8069f65961392f53f3d383ea6422398cfb
Eugene Larkin <eugenelarkin66@outlook.com>
2020-04-24 00:03:50 -0400

    Docker Changes App Working On Docker

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 
-
 echo "serving  delicious..."
 # cd /delicious && ./node_modules/nodemon/bin/nodemon.js --inspect=0.0.0.0:8080 --watch ./app ./start.js --ignore public/ 
 cd /delicious && npm start
